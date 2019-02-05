c6e2747f19bd36763d6f3f5545023763465bd5c8
decimal <alok.agrawal@decimal.co.in>
2019-02-05 11:07:59 +0530

    Move

--- src/main/java/project1/T1.java
+++ src/main/java/project1/T1.java
@@ -1,8 +1,45 @@
 package project1;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import org.apache.poi.ss.usermodel.Cell;
+
 public class T1 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
+		
+		FileInputStream FI = new FileInputStream("//home//decimal//Automation//TestData//Test.xlsx");
+		
+		XSSFWorkbook WB = new XSSFWorkbook(FI);
+		XSSFSheet WS = WB.getSheetAt(0);
+		Row row = WS.getRow(0);
+		Cell column = row.getCell(1);
+		int rowcount = WS.getLastRowNum();
+		
+		for (int i=1;i<=rowcount;i++) {
+			
+			int colcount = WS.getRow(i).getLastCellNum();
+			
+			for (int j=0;j<colcount;j++)
+			{
+			
+			Cell Text = WS.getRow(i).getCell(j);
+			System.out.println(Text);
+			
+			}
+		
+			
+		}
+		
+		//System.out.println(column);
+		//System.out.println(rowcount);
+		
 		// TODO Auto-generated method stub
 		
 		System.out.println("hello");
