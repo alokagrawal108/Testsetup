56649bc966cb45fa425c0796345b3b62637e54d6
decimal <alok.agrawal@decimal.co.in>
2019-02-11 17:56:20 +0530

    11thFeb 6PM

--- src/test/java/project1/WebLaunch.java
+++ src/test/java/project1/WebLaunch.java
@@ -1,11 +1,15 @@
 package project1;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.Test;
 
 public class WebLaunch {
   @Test
   public void Launch() {
 	  
-	  System.out.println("        Trial Run-Automation setup     ");
+	
+	  
+	  System.out.println("Trial Run-Automation setup");
   }
 }
